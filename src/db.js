9866c2bfe8171f7685aef4738c27601dfb834324
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2022-10-06 10:28:57 -0300

    Update db.js
    Removed the unnecessary command lines

--- src/db.js
+++ src/db.js
@@ -1,43 +1,16 @@
-const mysql = require('mysql2/promise')
-
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "devbuscapet",
-//     password: "devbuscapet",
-//     database: "bdbuscapet"
-// });
-
-// connection.connect(function(err) {
-//     if (err) {
-//         console.error('error connecting:' + err.stack);
-//         return;
-//     }
-
-//     console.log('connected as id' + connection.threadId);
-// });
-
-// connection.query('SELECT * FROM produtos', function(err, result, fields) {
-//     if (err) 
-//         throw err;
-
-//     result.forEach(result => {
-//         console.log(result);
-//     });
-// })
-
-// connection.end();
+const mysql = require("mysql2/promise");
 
 async function connect() {
-    const connection = await mysql.createConnection("mysql://devbuscapet:devbuscapet@localhost:3306/bdbuscapet");
-    console.log("Conectou ao banco");
-    global.connection = connection;
-    return connection
+  const connection = await mysql.createConnection("mysql://devbuscapet:devbuscapet@127.0.0.1:3306/bdbuscapet");
+  console.log("Conectou ao banco");
+  global.connection = connection;
+  return connection;
 }
 
 async function selectEmpresas() {
-    const conn = await connect();
-    const [rows] = await conn.query('SELECT * FROM empresas');
-    return rows
+  const conn = await connect();
+  const [rows] = await conn.query('SELECT * FROM empresas');
+  return rows;
 }
 
-module.exports = {selectEmpresas}
+module.exports = { selectEmpresas };
