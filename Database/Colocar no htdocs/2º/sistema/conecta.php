a0aa67d8c8dc310f7dc51d9a20908eb05464660f
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2022-10-20 12:04:47 -0300

    Connecting new index.php to database

--- Database/Colocar no htdocs/2º/sistema/conecta.php
+++ Database/Colocar no htdocs/2º/sistema/conecta.php
@@ -1,3 +1,2 @@
 <?php
-    $conexao = mysqli_connect('localhost', 'devbuscapet', 'devbuscapet', 'bdbuscapet');
-?>
+    $conn = mysqli_connect('localhost', 'devbuscapet', 'devbuscapet', 'bdbuscapet');
