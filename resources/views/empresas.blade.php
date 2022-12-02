c8c3ee9042ce7d9d3d82c15e0dcf8ecd0dc832c0
Your Name <you@example.com>
2022-12-01 21:57:00 -0300

    Alteração página empresas

--- resources/views/empresas.blade.php
+++ resources/views/empresas.blade.php
@@ -4,10 +4,233 @@
 
 @section('content')
 
-@if($busca != null)
-<p>Buscando por: {{$busca}}</p>
-@endif
+<div class="content">
+    <br><br><br>
+    <div class="container-xl">
+        <div class="row">
+            <div class="col-md-15">
+                <div class="title">
+                    <h2 class="Title-services">Empresas Avaliadas</h2>
+                </div>
+                <br><br>
+                <div class="banner">
+                    <a href="/">
+                        <img src="./img/bannerhome.jpg" alt="banner" class="bannerserv">
+                    </a>
+                </div>
+                <br>
+                <div id="myCarousel" class="carousel-services slide" data-ride="carousel" data-interval="0">
+                    <!-- Wrapper for carousel items -->
+                    <div class="carousel-inner">
+                        <div class="item carousel-item active">
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <div class="thumb-wrapper">
 
+                                        <div class="img-box">
+                                            <img src="./img/SuperPet.jpg" class="img-fluid" alt="SuperPet">
+                                        </div>
+                                        <div class="thumb-content">
+                                            <h4>SuperPet</h4>
+                                            <div class="star-rating">
+                                                <ul class="list-inline">
+                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                </ul>
+                                            </div>
+                                            <p style="font-color:grey;"><i>R. Leonel Mosele, 99</i></p>
+                                            <br/>
+                                            <a href="http://wa.me" target="_blank" class="WhatsApp" rel="noopener noreferrer">WhatsApp</a>
+                                            <a href="https://www.google.com/maps/place/R.+Leonel+Mosele+-+Conc%C3%B3rdia,+SC,+89700-000/@-27.2393008,-52.0161829,17z/data=!3m1!4b1!4m5!3m4!1s0x94e381e69065eac3:0x21c356953017f156!8m2!3d-27.2393008!4d-52.0161829" target="_blank" class="Maps" rel="noopener noreferrer">Maps</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="thumb-wrapper">
 
+                                        <div class="img-box">
+                                            <img src="./img/PetCia.png" class="img-fluid" alt="Pet & Cia">
+                                        </div>
+                                        <div class="thumb-content">
+                                            <h4>Pet & Cia</h4>
+                                            <div class="star-rating">
+                                                <ul class="list-inline">
+                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                </ul>
+                                            </div>
+                                            <p style="font-color:grey;"><i>R. Inácio Magarinos, 24</i></p>
+                                            <br>
+                                            <a href="http://wa.me" target="_blank" class="WhatsApp" rel="noopener noreferrer">WhatsApp</a>
+                                            <a href="https://www.google.com/maps/place/R.+In%C3%A1cio+Magarinos+-+Gruta,+Conc%C3%B3rdia+-+SC,+89700-000/data=!4m2!3m1!1s0x94e37f09bc74430d:0xdab744753a6cedde?sa=X&ved=2ahUKEwjb4sDrvdn7AhV3ppUCHfo1AUwQ8gF6BAgZEAE" target="_blank" class="Maps" rel="noopener noreferrer">Maps</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="thumb-wrapper">
+
+                                        <div class="img-box">
+                                            <img src="./img/PetLove.png" class="img-fluid" alt="PetLove">
+                                        </div>
+                                        <div class="thumb-content">
+                                            <h4>PetLove</h4>
+                                            <div class="star-rating">
+                                                <ul class="list-inline">
+                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                </ul>
+                                            </div>
+                                            <p style="font-color:grey;"><i>R. Luís Olivo, 88</i></p>
+                                            <br>
+                                            <a href="http://wa.me" target="_blank" class="WhatsApp" rel="noopener noreferrer">WhatsApp</a>
+                                            <a href="https://www.google.com/maps/place/R.+Lu%C3%ADs+Olivo+-+Gruta,+Conc%C3%B3rdia+-+SC,+89705-232/data=!4m2!3m1!1s0x94e37f0950dec4a9:0xcfe656ed9cb029f1?sa=X&ved=2ahUKEwiOsvfUvdn7AhVGrZUCHVkTDPAQ8gF6BAgWEAE" target="_blank" class="Maps" rel="noopener noreferrer">Maps</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="thumb-wrapper">
+
+                                        <div class="img-box">
+                                            <img src="./img/MaisPet.png" class="img-fluid" alt="MaisPet">
+                                        </div>
+                                        <div class="thumb-content">
+                                            <h4>MaisPet</h4>
+                                            <div class="star-rating">
+                                                <ul class="list-inline">
+                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                </ul>
+                                            </div>
+                                            <p style="font-color:grey;"><i>R. Dionísio Boff, 517</i></p>
+                                            <br>
+                                            <a href="http://wa.me" target="_blank" class="WhatsApp" rel="noopener noreferrer">WhatsApp</a>
+                                            <a href="https://www.google.com/maps/place/R.+Dion%C3%ADsio+Boff+-+Centro,+Conc%C3%B3rdia+-+SC,+89700-000/data=!4m2!3m1!1s0x94e381d9b1063cb7:0xa379b74390f27345?sa=X&ved=2ahUKEwiSpeC2vdn7AhWxpZUCHWXyC_AQ8gF6BAgWEAE" target="_blank" class="Maps" rel="noopener noreferrer">Maps</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="carousel-inner">
+                        <div class="item carousel-item active">
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <div class="thumb-wrapper">
+
+                                        <div class="img-box">
+                                            <img src="./img/PitPet.png" class="img-fluid" alt="PitPet">
+                                        </div>
+                                        <div class="thumb-content">
+                                            <h4>PitPet</h4>
+                                            <div class="star-rating">
+                                                <ul class="list-inline">
+                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                </ul>
+                                            </div>
+                                            <p style="font-color:grey;"><i>R. Marechal Deod., 101</i></p>
+                                            <br/>
+                                            <a href="http://wa.me" target="_blank" class="WhatsApp" rel="noopener noreferrer">WhatsApp</a>
+                                            <a href="https://www.google.com/maps/place/R.+Mal.+Deodoro+-+Conc%C3%B3rdia,+SC,+89700-000/data=!4m2!3m1!1s0x94e37f648387e05d:0x9c4c243ff93f0d49?sa=X&ved=2ahUKEwjYma-dvdn7AhWoHrkGHZsvDlIQ8gF6BAgWEAE" target="_blank" class="Maps" rel="noopener noreferrer">Maps</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="thumb-wrapper">
+
+                                        <div class="img-box">
+                                            <img src="./img/petmania.png" class="img-fluid" alt="PetMania">
+                                        </div>
+                                        <div class="thumb-content">
+                                            <h4>PetMania</h4>
+                                            <div class="star-rating">
+                                                <ul class="list-inline">
+                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                </ul>
+                                            </div>
+                                            <p style="font-color:grey;"><i>R. 29 de Julho, 523</i></p>
+                                            <br>
+                                            <a href="http://wa.me" target="_blank" class="WhatsApp" rel="noopener noreferrer">WhatsApp</a>
+                                            <a href="https://www.google.com/maps/place/R.+Vinte+e+Nove+de+Julho+-+Conc%C3%B3rdia,+SC,+89700-000/@-27.2412822,-52.0448411,17z/data=!3m1!4b1!4m5!3m4!1s0x94e37f1348ddd1bf:0x44d045d8eb71504a!8m2!3d-27.2412822!4d-52.0448411" target="_blank" class="Maps" rel="noopener noreferrer">Maps</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="thumb-wrapper">
+
+                                        <div class="img-box">
+                                            <img src="./img/Doggers.png" class="img-fluid" alt="Doggers">
+                                        </div>
+                                        <div class="thumb-content">
+                                            <h4>Doggers</h4>
+                                            <div class="star-rating">
+                                                <ul class="list-inline">
+                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                </ul>
+                                            </div>
+                                            <p style="font-color:grey;"><i>R. do Comércio, 31</i></p>
+                                            <br>
+                                            <a href="http://wa.me" target="_blank" class="WhatsApp" rel="noopener noreferrer">WhatsApp</a>
+                                            <a href="https://www.google.com/maps/place/R.+do+Com%C3%A9rcio+-+Centro,+Conc%C3%B3rdia+-+SC,+89700-000/data=!4m2!3m1!1s0x94e37f64a3ef940d:0x62a0c20b028b2eb1?sa=X&ved=2ahUKEwiotaGSvNn7AhUOGLkGHQXNAYEQ8gF6BAgXEAE" target="_blank" class="Maps" rel="noopener noreferrer">Maps</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="thumb-wrapper">
+
+                                        <div class="img-box">
+                                            <img src="./img/DogFamily.jpg" class="img-fluid" alt="DogFamily">
+                                        </div>
+                                        <div class="thumb-content">
+                                            <h4>DogFamily</h4>
+                                            <div class="star-rating">
+                                                <ul class="list-inline">
+                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                    <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                </ul>
+                                            </div>
+                                            <p style="font-color:grey;"><i>R. Adolfo Konder, 110</i></p>
+                                            <br>
+                                            <a href="http://wa.me" target="_blank" class="WhatsApp" rel="noopener noreferrer">WhatsApp</a>
+                                            <a href="https://www.google.com/maps/place/R.+Adolfo+Konder+-+Centro,+Conc%C3%B3rdia+-+SC,+89700-000/data=!4m2!3m1!1s0x94e37f62f3e94c6f:0x71d36b00fe43ce58?sa=X&ved=2ahUKEwjj_b_qu9n7AhX8qZUCHYVABlgQ8gF6BAgSEAE" target="_blank" class="Maps" rel="noopener noreferrer">Maps</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+</div>
 
 @endsection
