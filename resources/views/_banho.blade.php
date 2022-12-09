a6663a1e052242ecdedf1171fe2ea30bba43483d
Pedro10hg <110560927+Pedro10hg@users.noreply.github.com>
2022-12-08 21:08:19 -0300

    Add files via upload

---
+++ resources/views/_banho.blade.php
@@ -0,0 +1,167 @@
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banho e Tosa</title>
+    <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <main>
+        <div class="content">
+            <br><br><br>
+                <div class="container-xl">
+            <div class="row">
+                <div class="col-md-4">
+                    <br><br>
+                <div class="banner">
+                    <img src="./img/Banho.jpg" alt="banner" class="bannerserv">
+                </div>
+            </div>
+            
+                <div class="col-md-8">
+                    <div class="title">
+                        <h2 class="Title-About">Banho para Porte Médio</h2>
+                    </div>
+                    <br><br>
+                    <div class ="box">
+                        <div class="star-rating">
+                            <ul class="list-inline">
+                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                            </ul>
+                        </div>
+                        <div class="prices">
+                        <p class="item-price"><b>R$89,90</b></p>
+                        </div>
+                        <br><br>
+                    <p class="text">
+                        
+                        A higiene é fundamental para a manutenção da saúde e bem-estar de nossos amigos de estimação. Portanto, criar o hábito de dar banho regularmente é extremamente importante para que os pets fiquem protegidos. 
+                        <br><br>
+                        Existem procedimentos corretos para que o banho seja bem realizado e o pet fique realmente limpo e higienizado.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <br><br><br><br>
+                    <center><h4 ce class="thumb-content">Disponível nas seguintes empresas</h4></center>
+                    <div id="myCarousel" class="carousel slide pointer-event" data-ride="carousel" data-interval="0">
+                        <div class="carousel-inner">
+                            <div class="item carousel-item active">
+                                <div class="row">
+                                                                        <div class="col-sm-3" style="margin-top: 15px; margin-bottom:15px;">
+                                                <div class="thumb-wrapper">
+                                                    <div class="img-box">
+                                                        <img src="./img/agropet.png" class="img-fluid" alt="agropet.png">
+                                                    </div>
+                                                    <div class="thumb-content">
+                                                        <h4>AgroPet</h4>
+                                                        <p>Rua das Abobrinhas, 221</p>
+                                                        <div class="star-rating">
+                                                            <ul class="list-inline">
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                            </ul>
+                                                        </div>
+                                                        <br>
+                                                        <a class="buttonClass-services" href="http://wa.me/554995798532" target="__blank">Whatsapp</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                                                            <div class="col-sm-3" style="margin-top: 15px; margin-bottom:15px;">
+                                                <div class="thumb-wrapper">
+                                                    <div class="img-box">
+                                                        <img src="./img/pet-love.png" class="img-fluid" alt="pet-love.png">
+                                                    </div>
+                                                    <div class="thumb-content">
+                                                        <h4>PetLove</h4>
+                                                        <p>Rua dos Coqueiros, 5432</p>
+                                                        <div class="star-rating">
+                                                            <ul class="list-inline">
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                            </ul>
+                                                        </div>
+                                                        <br>
+                                                        <a class="buttonClass-services" href="http://wa.me/552184185698" target="__blank">Whatsapp</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                                                            <div class="col-sm-3" style="margin-top: 15px; margin-bottom:15px;">
+                                                <div class="thumb-wrapper">
+                                                    <div class="img-box">
+                                                        <img src="./img/petecia.jpg" class="img-fluid" alt="pet&amp;cia.jpg">
+                                                    </div>
+                                                    <div class="thumb-content">
+                                                        <h4>Pet&amp;Cia</h4>
+                                                        <p>Avenida São Jorge, 234</p>
+                                                        <div class="star-rating">
+                                                            <ul class="list-inline">
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                            </ul>
+                                                        </div>
+                                                        <br>
+                                                        <a class="buttonClass-services" href="http://wa.me/554532144589" target="__blank">Whatsapp</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                                                            <div class="col-sm-3" style="margin-top: 15px; margin-bottom:15px;">
+                                                <div class="thumb-wrapper">
+                                                    <div class="img-box">
+                                                        <img src="./img/55fe6bb9b9305_thumb900.png" class="img-fluid" alt="55fe6bb9b9305_thumb900.png">
+                                                    </div>
+                                                    <div class="thumb-content">
+                                                        <h4>PetShop do Sérgio</h4>
+                                                        <p>Rua Abobrinhas, 432</p>
+                                                        <div class="star-rating">
+                                                            <ul class="list-inline">
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                                                                <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                                                            </ul>
+                                                        </div>
+                                                        <br>
+                                                        <a class="buttonClass-services" href="http://wa.me/554998745632" target="__blank">Whatsapp</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+                
+                </a>
+                            
+                        </div>
+                            </div>
+                        </div>
+                    </div>
+                        
+                    
+                
